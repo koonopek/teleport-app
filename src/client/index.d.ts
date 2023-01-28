1350eb2d624551a5aa96a03f5571b0392a308715
Michał Konopka <konopek68@gmail.com>
2023-01-28 10:16:13 +0100

    feat: update address to mumbai

---
+++ src/client/index.d.ts
@@ -0,0 +1,7 @@
+export * from './Buyer';
+export * from './Seller';
+export * from './Offers';
+export * from './TeleportEscrow';
+export * from './ERC20';
+export * from './Constants';
+//# sourceMappingURL=index.d.ts.map
